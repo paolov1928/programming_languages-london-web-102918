29e8154dae84eeb583bd1c5e6cab80c3941e8600
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-17 16:38:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,6 @@
 def reformat_languages(languages)
   # your code here
+  #iterate over and maybe reassign like the big shoe rebound one
+  #Could create a little array with each part of language and then reassign to the hash
+  #use the to symb changer
 end
